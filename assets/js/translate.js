fb82a956055146f3a258d940b61744d73f82f1cf
sheylabre <sheylabrena@hotmail.com>
2019-10-16 01:29:59 -0500

    trasnlate

---
+++ assets/js/translate.js
@@ -0,0 +1,162 @@
+/*
+var languages = {};
+$("[data-text-key]").each(function (index, item) {
+	var $item = $(item)
+    languages[$item.data("text-key")] = {
+      "en": $item.html().trim(),
+      "es": "",
+    }
+});
+JSON.stringify(languages);
+*/
+
+var languages = {
+    "MenuHome": {
+        "en": "Home",
+        "es": ""
+    },
+    "MenuServices": {
+        "en": "Services",
+        "es": ""
+    },
+    "MenuProjects": {
+        "en": "Projects",
+        "es": ""
+    },
+    "MenuContact": {
+        "en": "Contact",
+        "es": ""
+    },
+    "HomeSlogan": {
+        "en": "Marine and Civil Architectural Solutions",
+        "es": ""
+    },
+    "AboutUs": {
+        "en": "About Us",
+        "es": ""
+    },
+    "FullService": {
+        "en": "FULL SERVICE CONTRACTOR IN MARINE AND CIVIL ARCHITECTURAL SOLUTIONS",
+        "es": ""
+    },
+    "TextAbout": {
+        "en": "V&amp;CH Global is a firm based in Lima, Peru that provides innovative solutions, qualitymaterials and services to build new spaces and refurbish cruises and on-land projects.",
+        "es": ""
+    },
+    "TextG": {
+        "en": "Globalization",
+        "es": ""
+    },
+    "TextD": {
+        "en": "Design",
+        "es": ""
+    },
+    "TextT": {
+        "en": "Team",
+        "es": ""
+    },
+    "TextP": {
+        "en": "Puntuality",
+        "es": ""
+    },
+    "OurServices": {
+        "en": "Our services",
+        "es": ""
+    },
+    "ServicesDe": {
+        "en": "Design and building",
+        "es": ""
+    },
+    "ServicesCu": {
+        "en": "Customize manufacturing",
+        "es": ""
+    },
+    "ServicesPro": {
+        "en": "Procurement",
+        "es": ""
+    },
+    "ServicesTu": {
+        "en": "Turn-Key Services",
+        "es": ""
+    },
+    "ServicesIm": {
+        "en": "Import/Export",
+        "es": ""
+    },
+    "ServicesCo": {
+        "en": "Consulting",
+        "es": ""
+    },
+    "OurProjects": {
+        "en": "Our projects",
+        "es": ""
+    },
+    "LeerMas": {
+        "en": "See more &gt;&gt;",
+        "es": ""
+    },
+    "OurAllies": {
+        "en": "Our<br>Allies",
+        "es": ""
+    },
+    "InsMail": {
+        "en": "Institutional Mail",
+        "es": ""
+    },
+    "phone": {
+        "en": "<i class=\"fas fa-fw fa-phone-alt\"></i>Phone",
+        "es": ""
+    },
+    "VisitPopup": {
+        "en": "Visit us at CasaCor 2019 until November 03<sup class=\"lower-case\">rd</sup>",
+        "es": ""
+    },
+    "Send": {
+        "en": "Send",
+        "es": ""
+    },
+    "TextServicesDe": {
+        "en": "Our work begins from the first interaction with the clients to assist in thedevelopingof their ideas providing the best cost-efficient and quality solutions. Our servicesinclude the presentation of the Project General Concept, followed by the elaborationofconstruction documents: Architectural, Engineering, Planning, Procurement,Monitoring,and Delivery of the final product.",
+        "es": ""
+    },
+    "TextServicesCu": {
+        "en": "Our target is INNOVATION, we focus our work and global experience providingcustomized solutions for the broad spectrum of the Marine and Civil BuildingProjects. Our architectural playground exposes us to assist our clients in achievingtheir challenging requirements with creative ideas.",
+        "es": ""
+    },
+    "TextServicesPro": {
+        "en": "We provide access to a global network of vendors to find material and services thatare not common in the Latin American market.",
+        "es": ""
+    },
+    "TextServicesTu": {
+        "en": "We facilitate installation worldwide, from specialized labor, to turnkey solutionsfrom start to finish using qualified installers in different fields: Ceiling,Flooring, Carpentry, Welding, Painting, Fittings, Coverings, Mechanical Engineering,and more.",
+        "es": ""
+    },
+    "TextServicesIm": {
+        "en": "We provide Logistic Services to import and export merchandize from/to around the World.We have an experienced team working everyday with Vendors and Freight Forwarders who wehave international alliances to meet deadlines and ensure the right handling of goods.",
+        "es": ""
+    },
+    "TextServicesCo": {
+        "en": "We count with a team of professionals in the Architecture, Engineering, andManagement field to provide adequate support and advise with different perspectivesto solve problems and improve their business.",
+        "es": ""
+    },
+    "ContactUs": {
+        "en": "Contact Us",
+        "es": ""
+    },
+    "Company": {
+        "en": "Company<input type=\"text\" name=\"company\" class=\"padding-horizontal-2\" required=\"\">",
+        "es": ""
+    },
+    "Cell": {
+        "en": "Cellphone<input type=\"text\" name=\"phone\" class=\"padding-horizontal-2\" required=\"\">",
+        "es": ""
+    },
+    "Message": {
+        "en": "Message<textarea name=\"message\" rows=\"5\" class=\"padding-horizontal-2\"></textarea>",
+        "es": ""
+    },
+    "Social": {
+        "en": "Social networks",
+        "es": ""
+    }
+}
